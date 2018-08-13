5cf3cb1f1659bcef26c88f79325aa413fee61c55
HenryG <glueck.h@googlemail.com>
2018-08-13 23:19:05 +0200

    initialization of repo

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Smart-Pump-Firmware
+# Smart Pump Firmware
+
+## Summary
+
+Smart Pump Firmware is the counter part of the project Smart Pump Backend (https://github.com/h3nr1-g/Smart-Pump-Backend).
+
+This repository provides the source code for a ESP8266 micro controller allows the ESP to communicate with the backend system via the defined REST API.
+
+
+## Brief Firmware Description
+
+The ESP initialize it self and tries to establish a WiFi connection. If this is successful, the ESP downloads from the backend system two timing values. The first value is the amount of seconds which the ESP should pull the the preconfigured output pin to HIGH. After this time period the ESP goes into the deep sleep mode. The duration of the sleep mode if defined by second value. When the ESP wakes up, it starts with the initalization phase again.
+
+
+## How To Set Up The Firmware
+
+```
+sudo apt-get install virtualenv python-pip python-dev
+git clone https://github.com/h3nr1-g/Smart-Pump-Firmware.git ~/sp-firmware
+cd sp-firmware
+./setup.sh
+```  
---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "############################################"
+echo "# Smart Pump Firmware Project Setup Script #"
+echo "############################################"
+echo ""
+
+echo "Check for command virtualenv"
+which virtualenv
+if [ $? -ne 0 ]; then
+    echo "Command virtualenv not available. Abort"
+	exit 1
+fi
+
+echo "Check for command pip"
+which virtualenv
+if [ $? -ne 0 ]; then
+    echo "Command pip not available. Abort."
+	exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo "Create virtual environment"
+virtualenv -p $(which python2) $SCRIPT_DIR/venv
+if [ $? -ne 0 ]; then
+    echo "Creation of virtual environment '$SCRIPT_DIR/venv' failed. Abort."
+	exit 1
+fi
+
+echo "Install dependencies from $SCRIPT_DIR/requirements.txt"
+source $SCRIPT_DIR/venv/bin/activate 
+pip install -r $SCRIPT_DIR/requirements.txt
+if [ $? -ne 0 ]; then
+    echo "Installation of dependencies failed. Abort."
+	exit 1
+fi
+
+echo "Create platformio project"
+PROJECT_DIR=$SCRIPT_DIR/firmware
+# IDEs supported by platformio: atom|clion|codeblocks|eclipse|emacs|netbeans|qtcreator|sublimetext|vim|visualstudio|vscode
+mkdir $PROJECT_DIR && platformio init -b nodemcu -d $PROJECT_DIR --ide clion
+if [ $? -ne 0 ]; then
+	echo "Creation of platformio project in directory $PROJECT_DIR failed. Abort."
+	exit 1
+fi
+		
+rm -rf $PROJECT_DIR/src
+ln -s $SCRIPT_DIR/src $PROJECT_DIR/src
+
+echo "Initialization of platformio project in directory $PROJECT_DIR FINISHED."
